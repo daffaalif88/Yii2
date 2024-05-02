56048b02cd767cc0649fe2123b38a52ee04a1e64
daffaalif <adaffa059@gmail.com>
2024-05-03 01:26:48 +0700

    update footer dan header

---
+++ frontend/views/layouts/footer.php
@@ -0,0 +1,10 @@
+<?php
+
+use yii\bootstrap5\Html;
+?>
+<footer class="footer mt-auto py-3 text-muted">
+    <div class="container">
+        <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+        <p class="float-end"><?= Yii::powered() ?></p>
+    </div>
+</footer>
