ff2d6f8542b138b64990ff4badd1f6b38c6d2fe1
daffaalif88 <adaffa059@gmail.com>
2024-05-22 16:27:04 +0700

    yang kurang tinggal form select2

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,8 +13,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'path/to/bootstrap-select.min.css',
     ];
     public $js = [
+        'path/to/bootstrap-select.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
